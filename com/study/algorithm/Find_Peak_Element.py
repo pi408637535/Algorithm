73ea04ad5bf86e5fee9ba9143500c112d9b420a6
piguanghua <piguanghua@163.com>
2020-06-02 20:27:42 +0800

    Find_Peak_Element finish

---
+++ com/study/algorithm/Find_Peak_Element.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #最少有三个元素
+
+        if len(nums) == 1:
+            return 0
+
+        if len(nums) == 2:
+            if nums[0] > nums[1]:
+                return 0
+            else:
+                return 1
+
+
+        start = 0
+        end = len(nums) -1
+
+        while start + 1 < end: #相等或相邻
+            mid = int( (start + end) / 2 )
+            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid-1]:
+                return mid
+            elif nums[mid-1] < nums[mid] and nums[mid] < nums[mid+1]:
+                start = mid
+            else:
+                end = mid
+
+        if start+1 == end and start == 0:
+            if nums[0] > nums[end]:
+                return 0
+        elif end + 1 == len(nums):
+            if nums[end] > nums[end-1]:
+                return end
+
+if __name__ == '__main__':
+    print(Solution().findPeakElement([1,2,3]))
