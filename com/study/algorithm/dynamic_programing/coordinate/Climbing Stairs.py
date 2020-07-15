3c268d11d32d5f271f71aca185a5dadafdad7b68
piguanghua <piguanghua@163.com>
2020-07-15 20:20:53 +0800

    Climbing Stairs

---
+++ com/study/algorithm/dynamic_programing/coordinate/Climbing Stairs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        f = [0 for i in range(n+1)]
+        f[0] = 1
+        f[1] = 1
+
+        for i in range(2,n+1):
+            f[i] = f[i-1] + f[i-2]
+
+        return f[n]
+
+
+
+
+if __name__ == '__main__':
+    n = 4
+    print(Solution().climbStairs(n))
