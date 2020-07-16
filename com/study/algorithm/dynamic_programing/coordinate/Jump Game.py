3492cb76a78fa5fe5fe40568769fdb13e961a0bb
piguanghua <piguanghua@163.com>
2020-07-16 15:16:07 +0800

    Jump Game II

--- com/study/algorithm/dynamic_programing/coordinate/Jump Game.py
+++ com/study/algorithm/dynamic_programing/coordinate/Jump Game.py
@@ -24,6 +24,35 @@ class Solution(object):
         return f[-1]
 
 
+class Solution1(object):
+    def canJump(self, nums):
+        n = len(nums)
+
+        if n == 1:
+            return True
+
+        #f = [False for i in range(n)]
+
+        #f[0] = True
+        i = 0
+        j = 0
+        while i < n:
+            j = nums[j] + j
+            if j >= n-1:
+                break
+            else:
+                i+=1
+        if j >= n:
+            return True
+        else:
+            return False
+
+
 if __name__ == '__main__':
+    #nums = [2,3,1,1,4]
+    #nums = [3,2,1,0,4]
+    #nums = [2,0]
+    #nums = [2, 0, 0]
     nums = [2,3,1,1,4]
-    print( Solution().canJump(nums) )
+
+    print( Solution1().canJump(nums) )
