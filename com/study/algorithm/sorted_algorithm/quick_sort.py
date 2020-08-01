b41c273fcbbbfeccb5ba2ff1c88034db6d82aec6
piguanghua <piguanghua@163.com>
2020-08-01 21:42:12 +0800

    quick sort

---
+++ com/study/algorithm/sorted_algorithm/quick_sort.py
@@ -0,0 +1,31 @@
+def quickSort(nums, left, right):
+    if left < right:
+        partition_index = partition(nums, left, right)
+
+        quickSort(nums, left, partition_index -1)
+        quickSort(nums, partition_index + 1, right)
+    return nums
+
+def partition(nums, left, right):
+    pivot = left
+    index = pivot + 1
+    i = index
+    while i <= right:
+        if nums[i] < nums[pivot]:
+            swab(nums, i, index)
+            index += 1
+        i += 1
+
+    swab(nums, pivot, index - 1)
+    return index - 1
+
+
+def swab(nums, i, j):
+    nums[i], nums[j] = nums[j], nums[i]
+
+
+
+if __name__ == '__main__':
+    nums = [3,46,7,8,11,2,1]
+    quickSort(nums, 0, len(nums) - 1)
+    print(nums)
