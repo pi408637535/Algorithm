8485b79da322993dac31a2246d679ede9927409f
piguanghua <piguanghua365@163.com>
2020-12-18 12:00:04 +0800

    122. Best Time to Buy and Sell Stock II  add dp function

--- com/study/algorithm/daily/122. Best Time to Buy and Sell Stock II.py
+++ com/study/algorithm/daily/122. Best Time to Buy and Sell Stock II.py
@@ -16,6 +16,27 @@ class Solution(object):
 
         return res
 
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        f = [[ 0 for j in range(2)] for i in range(len(prices))]
+        f[0][1] = -prices[0]
+
+        for index, ele in enumerate(prices):
+            if index == 0:
+                f[index][1] = -ele
+            else:
+                f[index][0] = max(f[index - 1][0], f[index-1][1] + ele)
+                f[index][1] = max(f[index - 1][1], f[index-1][0] - ele)
+
+        return f[index][0]
+
 if __name__ == '__main__':
     princes = [7,1,5,3,6,4]
     print(Solution().maxProfit(princes))
