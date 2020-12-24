54f93f603128b11c44d54545b47cc0d209faf543
piguanghua <piguanghua365@163.com>
2020-12-24 21:24:13 +0800

    12.24

---
+++ com/study/algorithm/daily/135. Candy.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+
+        left = [1] * len(ratings)
+        for i in range(1, len(ratings)) :
+            if ratings[i-1] < ratings[i]:
+                left[i] += 1
+
+
+        right = [1] * len(ratings)
+        for i in range(len(ratings)- 2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                right[i] += 1
+
+        res = []
+        for ele in zip(left, right):
+            res.append(max(ele))
+
+        return sum(res)
+
+if __name__ == '__main__':
+    rating = [1,0,2]
+    # rating = [1, 2, 2]
+    rating = [1,2,87,87,87,2,1]
+    print(Solution().candy(rating))
