09bc8cd6368295033142bdd3d354f5481dff1698
piguanghua <piguanghua@163.com>
2020-12-29 23:16:42 +0800

    12.29

--- com/study/algorithm/daily/135. Candy.py
+++ com/study/algorithm/daily/135. Candy.py
@@ -8,13 +8,13 @@ class Solution(object):
         left = [1] * len(ratings)
         for i in range(1, len(ratings)) :
             if ratings[i-1] < ratings[i]:
-                left[i] += 1
+                left[i] = left[i - 1] + 1
 
 
         right = [1] * len(ratings)
         for i in range(len(ratings)- 2, -1, -1):
             if ratings[i] > ratings[i+1]:
-                right[i] += 1
+                right[i] = right[i + 1] + 1
 
         res = []
         for ele in zip(left, right):
@@ -22,8 +22,10 @@ class Solution(object):
 
         return sum(res)
 
+
+
 if __name__ == '__main__':
     rating = [1,0,2]
     # rating = [1, 2, 2]
-    rating = [1,2,87,87,87,2,1]
+    # rating = [1,2,87,87,87,2,1]
     print(Solution().candy(rating))
--- com/study/algorithm/daily/714. Best Time to Buy and Sell Stock with Transaction Fee.py
+++ com/study/algorithm/daily/714. Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -7,23 +7,21 @@ class Solution(object):
         """
         if not prices:
             return 0
+        state = 2
+        f = [[0 for j in range(state) ] for i in range(len(prices))]
+        f[0][1] = -prices[0]
 
-        res = 0
-        pre = -1
-        min_num = prices[0]
-        for ele in prices:
-            if ele - min_num > fee and min_num != pre:
-                res += ele - min_num - fee
-                pre = min_num
+        for i,ele in enumerate(prices):
 
-            min_num = min(min_num, ele)
+            if i == 0:
+                f[i][1] = ele
+            else:
+                f[i][0] = max(f[i - 1][0], f[i - 1][1] + ele - fee)
+                f[i][1] = max(f[i - 1][1], f[i - 1][0] - ele)
 
-
-        return res
+        return f[i - 1][0]
 
 if __name__ == '__main__':
     prices = [1, 3, 2, 8, 4, 9]
     fee = 2
-    prices = [1,3,7,5,10,3]
-    fee = 3
-    print(Solution().maxProfit(prices, fee))
+    print( Solution().maxProfit(prices, fee) )
