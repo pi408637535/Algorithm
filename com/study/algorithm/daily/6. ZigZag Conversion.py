cb9bce216fc8ca4a58b9f4bd691a9706425e19ac
piguanghua <piguanghua365@163.com>
2021-01-27 21:23:34 +0800

    1.27

--- com/study/algorithm/daily/6. ZigZag Conversion.py
+++ com/study/algorithm/daily/6. ZigZag Conversion.py
@@ -7,22 +7,32 @@ class Solution(object):
         :rtype: str
         """
         res = [[] for i in range(numRows)]
-        directions = [(1,0),(-1,1)]
-        flag = 0 # 0, 1
-        for i in range(len(s)):
 
-            while flag == 0:
-                if i + directions[flag][0] < numRows:
-                    res[i % numRows].append(s[i])
-                    i += 1
-                else:
-                    flag = 1
-            j = numRows - 1 - 1
-            while flag == 1:
-                if 0 < i + directions[flag][0] and j  :
+        index = 0
+        length = len(s)
+        while index < length:
+            i = 0
+            while i < numRows and index < length: #只要是和数组相关就要确保不越界。这是个编程习惯
+                res[i].append(s[index])
+                index += 1
+                i += 1
+
+            i = numRows - 2 #规律： 2-0，3-1，4-2，5-3
+            while 0 < i and index < length: #这里需要确保 i > 0,因为只有这样才能确保方向进入向下的模式
+                res[i].append(s[index])
+                i -= 1
+                index += 1
+
+        content = []
+        for i in range(numRows):
+            content.append("".join(res[i]))
+
+        return "".join(content)
 
 
 
 if __name__ == '__main__':
     string = "PAYPALISHIRING"
-    print(len(string))
+    numRows = 4
+    #"PINALSIGYAHRPI"
+    print(Solution().convert(string, numRows))
